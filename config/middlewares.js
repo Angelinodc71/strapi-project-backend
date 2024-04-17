d0ca2486298e13e546fc4599ead15538ef1a23cb
Angelinodc71 <angelinodc75@gmail.com>
2024-04-17 23:17:01 +0200

    initial commit

---
+++ config/middlewares.js
@@ -0,0 +1,20 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: '*',
+      methods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+      headers: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      credentials: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
